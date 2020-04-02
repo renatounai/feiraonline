e70a1136e4793d229abb882f7a56560223d27d37
renatounai <54586557+renatounai@users.noreply.github.com>
2020-04-02 19:59:12 -0300

    Alteração no Controller para se adequar melhor aos padrões REST.

--- src/main/java/br/com/controller/CategoriaController.java
+++ src/main/java/br/com/controller/CategoriaController.java
@@ -8,12 +8,14 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.modelo.Categoria;
 import br.com.repositorio.CategoriaRepository;
 
 @RestController
+@RequestMapping("/categorias")
 public class CategoriaController {
 	
 	@Autowired
@@ -24,22 +26,22 @@ public class CategoriaController {
 	 * Esse método irá retornar todas as categorias.
 	 * @return
 	 */
-	@GetMapping(value = "/categorias")
+	@GetMapping
 	public List<Categoria> selecionarTodasAsCategorias() {
 		return categoriaRepository.findAll();
 	}
 	
-	@GetMapping(value = "/categoria/{id}")
+	@GetMapping(value = "/{id}")
 	public Categoria selecionarCategoriaPeloId(@PathVariable int id) {
 		return categoriaRepository.findById(id).get();
 	}
 	
-	@PostMapping(value = "/categoria")
+	@PostMapping
 	public void salvarCategoria(@RequestBody Categoria categoria) {
 		categoriaRepository.save(categoria);
 	}
 	
-	@DeleteMapping(value = "/categoria/{id}")
+	@DeleteMapping(value = "/{id}")
 	public void deletarCategoria(@PathVariable int id) {
 		Categoria categoria = categoriaRepository.findById(id).get();
 		categoriaRepository.delete(categoria);
